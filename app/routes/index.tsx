850d43d8bbde97a7eb86f84a05f7bbbed09850ac
Lars Houmark <lars@houmark.com>
2022-08-31 21:03:24 -0500

    Fix iOS height and improve margins

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,9 @@
 export default function Index() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-black/50 text-white">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-black/50 text-white min-h-screen-ios h-screen-ios">
       <div>
-        <h1 className="main-headline">houmark.com</h1>
-        <p className="mt-2 rounded-2xl bg-black/40 p-6 text-center text-3xl">
+        <h1 className="main-headline m-4 sm:m-0">houmark.com</h1>
+        <p className="m-4 mt-2 rounded-2xl bg-black/40 p-6 text-center text-3xl sm:m-0">
           New site underway!{' '}
           <a
             href="mailto:info@houmark.com"
