cd8dee362798e2f8daf5b7ba8170032f27087c05
Lars Houmark <lars@houmark.com>
2022-09-01 23:54:30 -0500

    Don't "animate" on load, tweak design for CTA. Background a bit darker

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,23 @@
 export default function Index() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-black/50 text-white min-h-screen-ios h-screen-ios">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-black/60 text-white min-h-screen-ios h-screen-ios">
       <div>
         <h1 className="main-headline m-4 sm:m-0">houmark.com</h1>
-        <p className="m-4 mt-2 rounded-2xl bg-black/40 p-6 text-center text-3xl sm:m-0">
-          New site underway!{' '}
-          <a
-            href="mailto:info@houmark.com"
-            className="group transition-all duration-300 ease-in-out"
-          >
-            <span className="bg-gradient-to-r from-white to-white bg-[length:0%_3px] bg-left-bottom bg-no-repeat transition-all duration-500 ease-out group-hover:bg-[length:100%_3px]">
-              Get in touch
-            </span>
-          </a>
-          .
-        </p>
+        <div className="m-4 mt-2 rounded-2xl bg-black/40 p-6 text-center text-3xl sm:m-0">
+          <p>New site underway!</p>
+          <p className="mt-4">
+            <a
+              href="mailto:info@houmark.com"
+              className="group transition-[background] duration-300 ease-in-out"
+            >
+              →{' '}
+              <span className="bg-gradient-to-r from-white to-white bg-[length:0%_3px] bg-left-bottom bg-no-repeat transition-[background] duration-500 ease-out group-hover:bg-[length:100%_3px]">
+                Get in touch
+              </span>
+            </a>
+            !
+          </p>
+        </div>
       </div>
     </div>
   );
