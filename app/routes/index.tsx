bc2aa24c3c1896cfe7740041b3956baca893ac96
Lars Houmark <lars@houmark.com>
2022-09-03 18:57:25 -0500

    Add animation on load

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,23 @@
+import { motion } from 'framer-motion';
+
 export default function Index() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-black/60 text-white min-h-screen-ios h-screen-ios">
       <div>
-        <h1 className="main-headline m-4 sm:m-0">houmark.com</h1>
-        <div className="m-4 mt-2 rounded-2xl bg-black/40 p-6 text-center text-3xl sm:m-0">
+        <motion.h1
+          initial={{ filter: 'blur(20px)' }}
+          animate={{ filter: 'blur(0px)' }}
+          transition={{ delay: 0.5 }}
+          className="main-headline m-4 sm:m-0"
+        >
+          houmark.com
+        </motion.h1>
+        <motion.div
+          initial={{ filter: 'blur(20px)', y: -130 }}
+          animate={{ filter: 'blur(0px)', y: 0 }}
+          transition={{ delay: 0.8, duration: 0.6 }}
+          className="m-4 mt-2 rounded-2xl bg-black/40 p-6 text-center text-3xl sm:m-0"
+        >
           <p>New site underway!</p>
           <p className="mt-4">
             <a
@@ -17,7 +31,7 @@ export default function Index() {
             </a>
             !
           </p>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
