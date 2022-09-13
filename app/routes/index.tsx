6c172504f61a9a81586ee273a2ac81b291e94ccf
Lars Houmark <lars@houmark.com>
2022-09-13 17:59:21 -0500

    Add will-change to ensure blur animations are smooth in (Mobile)Safari

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
     <div className="flex h-screen w-full flex-col items-center justify-center bg-black/60 text-white min-h-screen-ios h-screen-ios">
       <div>
         <motion.h1
-          className="main-headline m-4 sm:m-0"
+          className="main-headline m-4 will-change-transform sm:m-0"
           initial={{ filter: 'blur(20px)' }}
           animate={{ filter: 'blur(0px)' }}
           transition={{ delay: 0.5 }}
@@ -15,7 +15,7 @@ export default function Index() {
           initial={{ filter: 'blur(20px)', y: -130 }}
           animate={{ filter: 'blur(0px)', y: 0 }}
           transition={{ delay: 0.8, duration: 0.6 }}
-          className="group m-4 mt-2 rounded-2xl bg-black/40 to-red-700 p-6 text-center text-3xl ring-white transition-[box-shadow,background-color] duration-300 hover:bg-black hover:ring-8 sm:m-0"
+          className="group m-4 mt-2 rounded-2xl bg-black/40 to-red-700 p-6 text-center text-3xl ring-white transition-[box-shadow,background-color] duration-300 will-change-transform hover:bg-black hover:ring-8 sm:m-0"
         >
           <p>New site underway!</p>
           <p className="mt-4 transition-colors delay-300 duration-1000 group-hover:text-green-500">
