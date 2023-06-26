3faf4e4496ae95550d44b5dc0340654efe034dd1
Lars Houmark <lars@houmark.com>
2023-06-25 22:56:48 -0500

    cross-origin on preconnect for fonts

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ interface Environment {
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+  { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous' },
   {
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Signika+Negative&display=swap',
