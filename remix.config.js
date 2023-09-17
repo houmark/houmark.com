9e7aad3ff7c3c672a26e84e021145d0f3209f23f
Lars Houmark <lars@houmark.com>
2023-09-16 19:40:07 -0500

    Update to Remix v2

--- remix.config.js
+++ remix.config.js
@@ -15,12 +15,4 @@ module.exports = {
   },
   ignoredRouteFiles: ['**/.*'],
   tailwind: true,
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_routeConvention: true,
-    v2_normalizeFormMethod: true,
-    v2_dev: true,
-    v2_headers: true,
-  },
 };
